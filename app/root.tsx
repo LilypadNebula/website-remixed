78f2cd715c0f6ccf89746646586cd44f37463d2d
Lily <lilyneedsanemail@gmail.com>
2022-03-14 21:24:17 -0400

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,33 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+import type { MetaFunction } from "remix";
+
+export const meta: MetaFunction = () => {
+  return { title: "Lily Eisner" };
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
