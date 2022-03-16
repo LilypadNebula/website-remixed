44e2e358209c86dae2ebad1a840639994ed7734f
Lily <lilyneedsanemail@gmail.com>
2022-03-15 23:52:25 -0400

    Fonts and pictures

--- app/root.tsx
+++ app/root.tsx
@@ -28,14 +28,17 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
-        <div>
-          <nav>
-            <NavLink to="home">Home</NavLink>
-            <NavLink to="blog">Blog</NavLink>
-            <NavLink to="contact">Contact Me</NavLink>
-          </nav>
-        </div>
+      <body className="flex h-screen">
+        <nav className="flex flex-col p-8 justify-between items-center h-full text-2xl font-heading shadow-lg">
+          <NavLink to="home">Home</NavLink>
+          <NavLink to="blog">Blog</NavLink>
+          <NavLink to="contact">Contact Me</NavLink>
+          <img
+            src="/big_logo.png"
+            alt="Pink circle logo with Lily Eisner in dark text"
+            className="h-48 w-48"
+          />
+        </nav>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: "Maler, display",
+        main: "Asap, san-serif",
+      },
+    },
   },
   plugins: [],
 };
