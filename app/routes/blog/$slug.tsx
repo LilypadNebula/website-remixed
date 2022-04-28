14b0354cace9bd0e6b5696451f553732a90d86f9
Lily <lilyneedsanemail@gmail.com>
2022-04-28 19:54:32 -0400

    Pulls from graphcms

---
+++ app/routes/blog/$slug.tsx
@@ -0,0 +1,43 @@
+import { useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { GraphQLClient } from "graphql-request";
+import { marked } from "marked";
+
+const graphcms = new GraphQLClient(
+  "https://api-us-east-1.graphcms.com/v2/cl2fdhzyj15jf01z66i2qd3oo/master"
+);
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const {
+    post: { title, content },
+  } = await graphcms.request(
+    `
+    query PostBySlug($slug: String!){
+      post(where: {slug: $slug}) {
+        id
+        title
+        content
+      }
+    }
+    `,
+    { slug: params.slug }
+  );
+
+  const html = marked(content);
+
+  return json({ title, html });
+};
+
+export default function PostSlug() {
+  const { title, html } = useLoaderData<{
+    title: string;
+    html: string;
+  }>();
+  return (
+    <div className="font-main text-zinc-100">
+      <p className="text-4xl">{title}</p>
+      <div dangerouslySetInnerHTML={{ __html: html }}></div>
+    </div>
+  );
+}
---
+++ app/routes/blog/index.tsx
@@ -0,0 +1,37 @@
+import { useLoaderData, Link } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { GraphQLClient } from "graphql-request";
+
+const graphcms = new GraphQLClient(
+  "https://api-us-east-1.graphcms.com/v2/cl2fdhzyj15jf01z66i2qd3oo/master"
+);
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const results = await graphcms.request(`
+    {
+      posts {
+        id
+        slug
+        title
+      }
+    }
+    `);
+
+  return json(results.posts);
+};
+
+export default function Blog() {
+  const results =
+    useLoaderData<{ id: string; slug: string; title: string }[]>();
+  return (
+    <div className="font-main text-zinc-100">
+      <p>Blog Posts</p>
+      {results.map((post) => (
+        <Link key={post.id} to={post.slug}>
+          {post.title}
+        </Link>
+      ))}
+    </div>
+  );
+}
